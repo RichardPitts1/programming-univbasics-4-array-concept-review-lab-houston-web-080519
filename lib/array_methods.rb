f116cf21355fcbacd5e2e816464379d257ea7079
Richard Pitts <richardepitts@gmail.com>
2019-07-23 23:36:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   # so if you pass in [1, 2, 3, 4, 5] it should return 2 if you pass in array, 3 #Pseudocode
-  if array.each |x| == value_to_find 
+  if array.each |x| = value_to_find 
     array(x)
   else
     return nil
